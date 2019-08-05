2ef3c5bb4072b13ebc94facaeac463ffd6484d08
guilhermeRangel <gui.vg16@hotmail.com>
2019-08-05 18:37:59 -0300

    criado ate o ponto anterior

--- headSoccer/headSoccer/GameScene.swift
+++ headSoccer/headSoccer/GameScene.swift
@@ -9,36 +9,71 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     
     var player = SKSpriteNode(imageNamed: "player")
     var playerTexture = SKTexture(imageNamed: "player")
     
+    var enemy = SKSpriteNode(imageNamed: "enemy")
+    var enemyTexture = SKTexture(imageNamed: "enemy")
+    
+    var ball = SKSpriteNode(imageNamed: "ball")
+    var ballTexture = SKTexture(imageNamed: "ball")
     
     override func didMove(to view: SKView) {
+       physicsWorld.contactDelegate = self
+        addElements()
+        
+        
+    }
+    
+    func addElements(){
         createSceneContents()
         createPlayer()
-        
-        
-        
+        createEnemy()
+        createBall()
     }
     
     func createPlayer(){
         player.anchorPoint = CGPoint(x: 0.5, y: 0.5)
-        player.position = CGPoint(x: 0, y: 0  )
+        player.position = CGPoint(x: -100, y: 0  )
         player.size = CGSize(width: 100, height: 100)
         player.name = "player"
         player.physicsBody?.affectedByGravity = true
         player.physicsBody?.isDynamic = true
-        player.physicsBody?.description
         player.physicsBody?.categoryBitMask = 0b1
-        player.physicsBody?.contactTestBitMask =  UInt32.max
+        player.physicsBody?.contactTestBitMask =  0b10
         player.physicsBody = SKPhysicsBody(texture: playerTexture, alphaThreshold: 0, size: player.size)
         addChild(player)
     }
     
+    func createEnemy(){
+        enemy.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        enemy.position = CGPoint(x: 200, y: 0  )
+        enemy.size = CGSize(width: 100, height: 100)
+        enemy.name = "enemy"
+        enemy.physicsBody?.affectedByGravity = true
+        enemy.physicsBody?.isDynamic = true
+        
+        enemy.physicsBody?.categoryBitMask = 0b10
+        enemy.physicsBody?.contactTestBitMask =  0b100
+        enemy.physicsBody = SKPhysicsBody(texture: enemyTexture, alphaThreshold: 0, size: enemy.size)
+        addChild(enemy)
+    }
     
+    func createBall(){
+        ball.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        ball.position = CGPoint(x: 0, y: 0  )
+        ball.size = CGSize(width: 50, height: 50)
+        ball.name = "ball"
+        ball.physicsBody?.affectedByGravity = true
+        ball.physicsBody?.isDynamic = true
+        ball.physicsBody?.categoryBitMask = 0b10
+        ball.physicsBody?.contactTestBitMask =  0b1
+        ball.physicsBody = SKPhysicsBody(texture: ballTexture, alphaThreshold: 0, size: ball.size)
+        addChild(ball)
+    }
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
@@ -47,7 +82,7 @@ class GameScene: SKScene {
     
     func createSceneContents() {
         self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
-        self.physicsWorld.gravity = CGVector(dx: 0, dy: -0.2)
+        self.physicsWorld.gravity = CGVector(dx: 0, dy: -0.5)
         
         //esse linha controla a tela
         self.physicsBody = SKPhysicsBody.init(edgeLoopFrom: self.frame)
@@ -73,12 +108,35 @@ class GameScene: SKScene {
                 print("dir")
             }
             
-            if location.y > player.position.y + 120 {
-                player.physicsBody?.applyImpulse(CGVector(dx: 120, dy: 0))
+            if location.y > player.position.y + 60 {
+                player.physicsBody?.applyImpulse(CGVector(dx: 30, dy: 30))
                 print("pular")
             }
         }
     }
     
-    
+    public func didBegin(_ contact: SKPhysicsContact) {
+        
+        guard let nodeA = contact.bodyA.node else {return}
+        guard let nodeB = contact.bodyB.node else { return}
+        
+        if((nodeA.name == "player" && nodeB.name == "ball") ||
+            (nodeA.name == "ball" && nodeB.name == "player")){
+          print("player colidiu com a bola")
+
+        }
+        
+        if((nodeA.name == "player" && nodeB.name == "enemy") ||
+            (nodeA.name == "enemy" && nodeB.name == "player")){
+            print("player colidiu com enemy")
+            
+        }
+        
+        if((nodeA.name == "enemy" && nodeB.name == "ball") ||
+            (nodeA.name == "ball" && nodeB.name == "enemy")){
+            print("enemy colidiu com bola")
+            
+        }
+    }
+
 }
